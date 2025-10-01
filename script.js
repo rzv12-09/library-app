21b92f4326c46f301e60cef903f7ec3069807d59
rzv12-09 <olariurazvan20@gmail.com>
2025-10-01 09:20:50 +0300

    Add sample books to library array

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function addBookToLibrary(title,author,pages,isRead) {
     let newBook = new Book(title,author,pages,isRead);
     myLibrary.push(newBook);
 }
+
+let book1 = new Book("My book","John",233,1);
+let book2 = new Book("Second Book","Andrei",313,0);
+
+myLibrary.push(book1);
+myLibrary.push(book2);
